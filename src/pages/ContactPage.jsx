6fd06edf619d1d9477605528e67f63b400fe5359
leticiaoliveira5 <olivaleticia@hotmail.com>
2025-08-14 19:54:16 -0300

    bg

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,7 @@ import Form from '../components/Form';
 
 function ContactPage() {
   return (
-    <section id="contato" className="py-16 px-6 bg-blue-700 text-white">
+    <section id="contato" className="py-16 px-6 bg-sky-700 text-white">
       <h2 className="text-3xl font-bold text-center mb-6">
         Fale com um de nossos consultores
       </h2>
