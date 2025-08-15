9f124e89c80e628fc35a9b0c477059cb0139df2e
leticiaoliveira5 <olivaleticia@hotmail.com>
2025-08-15 14:46:09 -0300

    adjust logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,11 +5,11 @@ import D from '../icons/D'
 const Logo = () => {
   return(
     <div className='inline-flex flex-row items-center align-middle'>
-      <R style={{marginRight: '-0.1em', zIndex: 97, fill: "#035682" }} />
-      <A style={{marginRight: '-0.1em', marginLeft: '-0.1em', zIndex: 98, fill: "#035682" }} />
-      <D style={{marginRight: '-0.1em', marginLeft: '-0.1em', zIndex: 99, fill: "#fb6a02" }} />
-      <A style={{marginRight: '-0.1em', marginLeft: '-0.1em', zIndex: 98, fill: "#035682" }} />
-      <R style={{marginLeft: '-0.1em', zIndex: 97, fill: "#035682" }} />
+      <R style={{marginRight: '-0.15em', zIndex: 97, fill: "#035682" }} />
+      <A style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 98, fill: "#035682" }} />
+      <D style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 99, fill: "#fb6a02" }} />
+      <A style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 98, fill: "#035682" }} />
+      <R style={{marginLeft: '-0.15em', zIndex: 97, fill: "#035682" }} />
     </div>
   )
 }
