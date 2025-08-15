88634b4c71f831e9895ca53e05c018607f8b25b1
Letícia Oliveira Rodrigues <olivaleticia@hotmail.com>
2025-08-15 14:24:52 -0300

    fix console errors

--- src/icons/D.jsx
+++ src/icons/D.jsx
@@ -3,7 +3,6 @@ const D = (props) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
-      height="auto"
       version="1.2"
       viewBox="0 0 810 1012.5"
       {...props}
