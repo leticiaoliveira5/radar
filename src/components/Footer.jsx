ce732dbba50c8e6a1f3fead0837f4ede61e7b39e
leticiaoliveira5 <olivaleticia@hotmail.com>
2025-08-15 15:47:33 -0300

    Make logo color customizable

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
+import Logo from '../components/Logo';
+
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-gray-300 py-6 text-center">
+      <p className='mb-3'><Logo color="#fff" /> ― Saúde · Seguros · Previdência </p>
       <p>
         © {new Date().getFullYear()} RADAR CORRETAGEM DE SEGUROS E SAÚDE LTDA ME — Todos os
         direitos reservados.
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,14 +2,18 @@ import R from '../icons/R';
 import A from '../icons/A';
 import D from '../icons/D'
 
-const Logo = () => {
+const Logo = ({color}) => {
+  const rColor = color || "#035682";
+  const aColor = color || "#035682";
+  const dColor = color || "#fb6a02";
+
   return(
     <div className='inline-flex flex-row items-center align-middle'>
-      <R style={{marginRight: '-0.15em', zIndex: 97, fill: "#035682" }} />
-      <A style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 98, fill: "#035682" }} />
-      <D style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 99, fill: "#fb6a02" }} />
-      <A style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 98, fill: "#035682" }} />
-      <R style={{marginLeft: '-0.15em', zIndex: 97, fill: "#035682" }} />
+      <R style={{marginRight: '-0.15em', zIndex: 97, fill: rColor }} />
+      <A style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 98, fill: aColor }} />
+      <D style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 99, fill: dColor }} />
+      <A style={{marginRight: '-0.15em', marginLeft: '-0.15em', zIndex: 98, fill: aColor }} />
+      <R style={{marginLeft: '-0.15em', zIndex: 97, fill: rColor }} />
     </div>
   )
 }
