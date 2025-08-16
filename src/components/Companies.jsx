1f40dbe57fb2055525b347730cd35c8a13ffeb8f
leticiaoliveira5 <olivaleticia@hotmail.com>
2025-08-15 21:00:50 -0300

    FINE LIST

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -3,24 +3,25 @@ import UnimedSvg from '../icons/UnimedSvg';
 import PortoSvg from '../icons/PortoSvg';
 import GoldenSvg from '../icons/GoldenSvg';
 
+const Item = ({icon, title, klass}) => {
+  return (
+    <div className={`bg-white shadow-lg rounded-lg p-8 flex flex-col items-center align-middle text-3xl ${klass}`} title={title}>
+      <a href="#contato">{icon}</a>
+    </div>
+  )
+};
+
 const Companies = () => {
 return (
   <div className='grid md:grid-cols-4 gap-8 text-2xl'>
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center align-middle" title="Amil">
-      <AmilSvg />
-    </div>
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center align-middle" title="Unimed">
-      <UnimedSvg />
-    </div>
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center align-middle" title="Porto Seguro">
-      <PortoSvg />
-    </div>
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center align-middle text-3xl" title="Golden Cross">
-      <GoldenSvg />
-    </div>
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center align-middle text-xl">
-      ...entre outras!
-    </div>
+    <Item icon={<AmilSvg />} title="Amil" />
+    <Item icon={<UnimedSvg />} title="Unimed" />
+    <Item icon={<PortoSvg />} title="Porto Seguro" />
+    <Item icon={<GoldenSvg />} title="Golden Cross" />
+    <Item icon={"Bradesco"} title="Bradesco" />
+    <Item icon={"Sulamérica"} title="Sulamérica" />
+    <Item icon={"Leve"} title="Leve" />
+    <Item icon={"...entre outras!"} klass="text-xl" />
   </div>
   )
 }
